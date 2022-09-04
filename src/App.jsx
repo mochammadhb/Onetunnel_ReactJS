b46dfbe0bf17ff0753b2ada8cf8228b6ca68fd90
mochammadhb <hdancowz@gmail.com>
2022-09-04 19:25:14 +0700

    kedua22

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./Layout";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/*" element={<Index />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
